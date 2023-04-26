0895a759afe9dfe6eefe8d58703952857fba9969
Alex Solohub <le03grande@gmail.com>
2023-04-26 15:29:51 +0300

    animation fixed

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const burgerButton = document.querySelector('.burger-button');
 const burgerMenu = document.querySelector('.menu');
 const main = document.querySelector('main');
 
+const typed = new Typed('#element', {
+  strings: ['serverless'],
+  typeSpeed: 150,
+  backSpeed: 150,
+  showCursor: false,
+  loop: true,
+});
+
 burgerButton.addEventListener('click', () => {
   burgerMenu.classList.toggle('visible');
   main.classList.toggle('hidden');
